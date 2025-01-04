2813b2cd8133c2eaecaa2c34cf1d38b82db715ac
cmc228 <cmc228@bath.ac.uk>
2025-01-04 10:13:11 +0000

    log in  to the webpage with the password and username

--- scraper.py
+++ scraper.py
@@ -19,5 +19,35 @@ def get_driver():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("disable-gpu")
-    options.add_argument("--no-sandbox")
+    options.add_argument("--no-sandbox")#sandbox security thing from chrome it could cause conflict with docker 
     return webdriver.Chrome(service= Service(ChromeDriverManager()))
+
+
+def scrape_jobs(url, username, password):
+    """
+    Logs into MyFuture and scrapes job postings.
+
+    Args:
+        url (str): URL of the job portal.
+        username (str): MyFuture username.
+        password (str): MyFuture password.
+
+    Returns:
+        list: A list of job postings as dictionaries.
+    """
+    # Initialize the WebDriver
+    driver = get_driver()  # Set up the Chrome WebDriver with the configured options
+
+    try:
+        driver.get(url)  # Open the  URL 
+        time.sleep(3)  # Wait for the page to load 
+
+        # Log in 
+        driver.find_element(By.ID, "username").send_keys(username)  #pass my username
+        driver.find_element(By.ID, "password").send_keys(password)  # andd password
+        driver.find_element(By.ID, "submitButton").click()  # click the login button to submit the form
+        time.sleep(5)  # Allow time for the login to process and redirect to the dashboard
+
+
+
+                
