286aa8d90fb48defdc98c243db3fd8f89009348e
cmc228 <cmc228@bath.ac.uk>
2025-01-05 09:15:19 +0000

    called main function in the main script

--- main.py
+++ main.py
@@ -16,9 +16,12 @@ def main():
 
     try:
 
+        print(f"USERNAME: {USERNAME}, PASSWORD: {PASSWORD}, JOB_URL: {JOB_URL}")
+
         scrape_jobs(JOB_URL,USERNAME, PASSWORD)
     except Exception as e:
         print(f"An error occured : {e }")
 
 
-        
+if __name__ == "__main__":
+    main()
