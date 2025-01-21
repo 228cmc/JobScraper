1760fa61f6e88ba16d4e19a12e4972682549e930
228cmc <228cmc@gmail.com>
2025-01-21 10:40:31 +0000

    save db now applied in main not in scraper

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-import os
 from config import JOB_URL, USERNAME, PASSWORD
 from scraper.scraper import scrape_jobs
-from db.database import create_database
-
+from db.database import create_tables, save_to_database, export_to_csv
 
 def main():
     """
-    main function to run the job scraper
+    Main function to scrape jobs, save to the database, and export to a CSV file.
     """
-    create_database()  # Initialize the database if it doesn't exist
-
     if not all([JOB_URL, USERNAME, PASSWORD]):
-        print(" missing sm in the config")
+        print("Missing some configuration values in the environment variables.")
         return
-    print("starting to scrap")
 
+    print("Starting the job scraper...")
+    jobs = scrape_jobs(JOB_URL, USERNAME, PASSWORD)  # Scrape jobs and return a list of dictionaries
 
-    try:
+    print("Initializing database...")
+    create_tables()  # Create tables if not already created
 
-        #print(f"USERNAME: {USERNAME}, PASSWORD: {PASSWORD}, JOB_URL: {JOB_URL}")
+    print("Saving jobs to the database...")
+    save_to_database(jobs)  # Save the jobs into the database
 
-        scrape_jobs(JOB_URL,USERNAME, PASSWORD)
-    except Exception as e:
-        print(f"An error occured : {e }")
+    print("Exporting jobs to CSV...")
+    export_to_csv()  # Export jobs from the database to a CSV file
 
+    print("Done! Jobs are saved and exported to CSV.")
 
 if __name__ == "__main__":
     main()
