ea2a2c52c360fb3887bdb5931e3654945b2a0922
228cmc <228cmc@gmail.com>
2025-01-21 10:01:53 +0000

    initialize the db in main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 from config import JOB_URL, USERNAME, PASSWORD
-from scraper import scrape_jobs
+from scraper.scraper import scrape_jobs
+from db.database import create_database
 
 
 def main():
     """
     main function to run the job scraper
     """
+    create_database()  # Initialize the database if it doesn't exist
 
     if not all([JOB_URL, USERNAME, PASSWORD]):
         print(" missing sm in the config")
